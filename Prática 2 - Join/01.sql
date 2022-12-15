95450dcb5b69a8c168565d8920203499dfc9f8f4
Douglas Semedo <dsemedoo@gmail.com>
2022-12-14 21:58:20 -0300

    Praticas 2 - formatação corrigida

--- Prática 2 - Join/01.sql
+++ Prática 2 - Join/01.sql
@@ -1,1 +1,5 @@
-SELECT users.name, users.id, cities.name FROM users JOIN cities ON users."cityId" = cities.id WHERE cities.name = 'Rio de Janeiro';
+SELECT users.name, users.id, cities.name
+ FROM users 
+ JOIN cities 
+ ON users."cityId" = cities.id 
+ WHERE cities.name = 'Rio de Janeiro';
